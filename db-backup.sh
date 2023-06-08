26cd9b545388240e22aa5b8c76361e86506a8eb7
rohithzak <134053766+rohithzak@users.noreply.github.com>
2023-06-08 08:20:02 +0530

    Update db-backup.sh

--- db-backup.sh
+++ db-backup.sh
@@ -8,11 +8,11 @@ pg_dump -U postgres -d db_name > /backup/$filename
 	if [ $? -eq 0 ]; then
          /usr/local/bin/aws --profile s3backup s3 cp /backup/$filename s3://cri-backups/db-backups/ 
         else
-		/usr/bin/python3 mail.py <sender mail ID> FAILED "SQL dump unsuccessful for hotcri"  # <sender mail ID> FAILED and sql sump unsuccessful are my parmaeter of choice, you can modify as per your requirement
+		/usr/bin/python3 mail.py <sender mail ID> FAILED "SQL dump unsuccessful for hotcri"  # FAILED and sql sump unsuccessful are my parmaeter of choice, you can modify as per your requirement
 		exit 1
         fi                                                             #Checks if the backup-process is completed successfully and sends mail in failure and push to s3 in success
 
-/usr/bin/python3 mail.py <Sender mail ID> SUCCESSFUL "Pushed to S3 Successfully"  #After pushing to s3, sends mail with success message
+/usr/bin/python3 mail.py <Sender mail ID> SUCCESSFUL "Pushed to S3 Successfully"  #After pushing to s3, sends mail with success message( You can give you parameter as per your preference)
 
 rm -f /backup/$filename    #Removes the backup from local server after pushing to s3
 
